1eb86614a8529be4791ca57709a7855e0ce302e4
Charlin_冯成林 <muxi@muxideiMac.local>
2015-03-05 22:12:50 +0800

    Initial Commit

---
+++ CoreTFManagerVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTFManagerVC
+//
+//  Created by 沐汐 on 15-3-5.
+//  Copyright (c) 2015年 沐汐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreTFManagerVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreTFManagerVC
+//
+//  Created by 沐汐 on 15-3-5.
+//  Copyright (c) 2015年 沐汐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
